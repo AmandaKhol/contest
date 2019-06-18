d51feb219cddf0da3d4f62e36809e7664e6958fa
Yamila Moreno <yamila.ms@gmail.com>
2019-06-18 19:44:25 +0200

    Initial version of the contest

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_cors import CORS
+
+from contest import file_name, try_number, check_file
+
+
+def setup_app():
+    app = Flask(__name__)
+    CORS(app=app, supports_credentials=True)
+
+    # SETUP URLS
+    app.add_url_rule('/try_number', methods=['POST'], view_func=try_number)
+    return app
+
+
+if __name__ == '__main__':
+    app = setup_app()
+    check_file()
+    app.run(host='0.0.0.0', port=5000, threaded=True)
+
+# python app.py
+# curl -X POST -H 'Content-Type: application/json' -d '{"number": 20}' http://localhost:5000/try_number
---
+++ contest.py
@@ -0,0 +1,78 @@
+import os.path
+
+from flask import jsonify, request
+
+file_name = 'saved_number'
+num = 0
+followers = 0
+password = 'hola'
+
+
+def try_number():
+    if not request.json:
+        return jsonify({'message': 'todo mal'}), 400
+
+    code = request.json.get('code', None)
+
+    if not code:
+        return jsonify({'message': 'send any code'}), 400
+
+    ok, number = get_number(code)
+    if not ok:
+        return jsonify({'message': 'is not correct'}), 200
+
+    global num
+    global followers
+    num = num + number
+    followers = followers + 1
+
+    update_file()
+
+    if not number_check(num, 11):
+        return jsonify({'message': 'prueba otra vez'}), 200
+
+    return jsonify({'message': 'has ganado'}), 200
+
+
+def check_file():
+
+    if os.path.exists(file_name):
+        with open(file_name, 'r') as f:
+            # 3,129 --> 3 followers, 129 num
+            global num
+            global followers
+            followers, num = f.readline().split(',')
+            num = int(num)
+            followers = int(followers)
+
+    else:
+        with open(file_name, 'w') as f:
+            f.write('0,0')
+
+
+def update_file():
+    with open(file_name, 'w') as f:
+        f.write('{},{}'.format(followers,num))
+
+
+def get_number(code):
+    code = code.lower()
+    if code.startswith(password):
+        _, number = code[0:len(password)], code[len(password):]
+        try:
+            number = int(number)
+            if number >= 100 and number < 1000:
+                return True, number
+            return False, None
+        except:
+            return False, None
+
+    return False, None
+
+
+def number_check(follower_number, number):
+    if follower_number%number == 0:
+        return True
+    else:
+        return False
+
